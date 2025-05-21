15a9662baafd586cf0a9e90612a710bb2a544cf4
LOLipop71 <vishruth.king@gmail.com>
2025-05-21 22:40:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,10 +11,10 @@ def home():
 @app.route('/users')
 def get_users():
     conn = psycopg2.connect(
-        host=os.environ['DB_HOST'],
-        dbname=os.environ['DB_NAME'],
-        user=os.environ['DB_USER'],
-        password=os.environ['DB_PASSWORD'],
+        host=aws-0-ap-southeast-1.pooler.supabase.com,
+        dbname=postgres,
+        user=postgres.slgsuvchudfsllbhuhid,
+        password=6wZsZYQpiRNanS_,
         port=5432
     )
     cur = conn.cursor()
