3bdd108447062b071dbe71ddd0d40b75ff9c62d7
LOLipop71 <vishruth.king@gmail.com>
2025-05-23 11:14:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ import json
 app = Flask(__name__)
 
 
-gunicorn_logger = logging.getLogger('gunicorn.error')
-app.logger.handlers = gunicorn_logger.handlers
-app.logger.setLevel(gunicorn_logger.level)
+# gunicorn_logger = logging.getLogger('gunicorn.error')
+# app.logger.handlers = gunicorn_logger.handlers
+# app.logger.setLevel(gunicorn_logger.level)
 
 
 @app.route('/')
